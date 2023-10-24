d319a1517e889f4a02807cb1da1610c2fc6bcc56
Achraf Taffah <99338171+TAFFAHACHRAF@users.noreply.github.com>
2023-10-24 13:07:30 +0200

    Delete src/main/java/org/example directory

--- src/main/java/org/example/observer/solution/Observable.java
+++
@@ -1,7 +0,0 @@
-package org.example.observer.solution;
-
-public interface Observable {
-    void addObserver(Observer obs);
-    void removeObserver(Observer obs);
-    void notifyObservers();
-}
--- src/main/java/org/example/observer/solution/Observer.java
+++
@@ -1,5 +0,0 @@
-package org.example.observer.solution;
-
-public interface Observer {
-    void update(Observable observable);
-}
